d52e888e389805163da75edb42a7c5c0e33a8aa6
Omkar Phadnis <114167955+omkarphadnis@users.noreply.github.com>
2022-10-11 23:18:58 +0530

    Add files via upload

---
+++ 11-10-2022/hierarchical_inheritance/accept.java
@@ -0,0 +1,19 @@
+//Hierarchical:
+
+//WAP to accept no from the user and perform the different types of Operator.
+package hierarchical_inheritance;
+
+import java.util.Scanner;
+
+public class accept 
+{
+	static int num1,num2;
+	public static void acc()
+	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter Two numbers");
+		num1=sc.nextInt();
+		num2=sc.nextInt();
+		
+	}
+}
